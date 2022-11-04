760ed862ce6c915d3f72f54587617079db7f88d9
zhamiila824 <jama.98.kjk@gmail.com>
2022-11-04 14:59:49 +0600

    add baseUrl to access token

--- wishlist/__init__.py
+++ wishlist/__init__.py
@@ -1,3 +1,9 @@
 import requests
 from authentification import get_token
 from config import baseURL
+
+
+def receiveSingleUser(user_id, access_token=get_token()):
+    r = requests.get(baseURL+'users/'+str(user_id),
+                     headers={'Authorization': 'Bearer {}'.format(access_token)})
+    return r
