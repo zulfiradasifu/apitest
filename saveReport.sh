72bd882eda947217f5d2793563d5a1a00d43a21a
zhamiila824 <jama.98.kjk@gmail.com>
2022-12-02 17:00:05 +0600

    update saveReport

--- saveReport.sh
+++ saveReport.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/Desktop/API_tests/ && \
+source venv/bin/activate && \
+pip3 install -r requirements.txt && \
 pytest -vvv . --html=reports/report.html --self-contained-html; \
-/usr/local/bin/aws s3 sync /home/ubuntu/Desktop/API_tests/reports/ s3://ssw-apitest-reports
+/usr/local/bin/aws s3 sync /home/ubuntu/Desktop/API_tests/reports/ s3://ssw-apitest-reports; \
+deactivate
