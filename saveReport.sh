94635ee35ef9495357827b1371e533d132ce7a67
zhamiila824 <jama.98.kjk@gmail.com>
2022-12-03 13:24:02 +0600

    add git pull

--- saveReport.sh
+++ saveReport.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/Desktop/API_tests/ && \
+git reset --hard; git clean -df; git pull; \
 source venv/bin/activate && \
 pip3 install -r requirements.txt && \
 pytest -vvv . --html=reports/report.html --self-contained-html; \
